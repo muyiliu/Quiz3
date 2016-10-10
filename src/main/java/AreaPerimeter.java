3613daafafa988fb19dd8225cec0fb993f31aa2e
detaoliu <detaoliu@idea-PC>
2016-10-09 23:09:48 -0400

    Second push

--- src/main/java/AreaPerimeter.java
+++ src/main/java/AreaPerimeter.java
@@ -5,13 +5,17 @@ public class AreaPerimeter extends GeometricObject{
 	public double side3 = 1;
 	
 
-
+	/*
+	 * set three sides for tranigle
+	 */
 	public AreaPerimeter(int side1,int side2,int side3){
 		this.side1 = side1;
 		this.side2 = side2;
 		 this.side3 = side3;
 	}
-	
+	/*
+	 * return to three sides' value
+	 */
 	public double getSide1(){
 		return side1;
 	}
@@ -22,19 +26,28 @@ public class AreaPerimeter extends GeometricObject{
 		public double getSide3(){
 			return side3;
 			}
-	
+	/*
+	 * set three sides again
+	 */
 	public void setThree_sides(double side1, double side2, double side3) {
 		this.side1 = side1;
 		this.side2 = side2;
 		this.side3 = side3;
 
-}
+}	
+	/*
+	 * (non-Javadoc)
+	 * @see GeometricObject#getPerimeter()
+	 */
 	public double getPerimeter(){
 		double perimeter = side1 + side2 + side3;
 		return perimeter;
 		
 	}
-	
+	/*
+	 * (non-Javadoc)
+	 * @see GeometricObject#getArea()
+	 */
 	public double getArea(){
 		double s = (getPerimeter())/2;
 		double area = Math.sqrt(s*(s-side1)*(s-side2)*(s-side3));
